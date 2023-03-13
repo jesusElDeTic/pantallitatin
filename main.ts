0fd71ed245d7fa1866161db37e6188f9be2915d3
jesusElDeTic <74316857+jesusElDeTic@users.noreply.github.com>
2023-03-13 23:28:36 +0100

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,62 @@
 input.onButtonPressed(Button.A, function () {
-    I2C_LCD1602.BacklightOff()
-    basic.pause(500)
-    I2C_LCD1602.BacklightOn()
-    I2C_LCD1602.ShowString("hola caracola", 0, 0)
+    if (pantalla < 3) {
+        pantalla += 1
+    } else {
+        pantalla = 1
+    }
+    makerbit.showStringOnLcd1602("" + (pantalla), makerbit.position1602(LcdPosition1602.Pos8), 6)
 })
+let pantalla = 0
+pantalla = 1
 I2C_LCD1602.LcdInit(39)
 I2C_LCD1602.clear()
+makerbit.lcdMakeCharacter(LcdChar.c1, makerbit.lcdCharacterPixels(`
+    # . # . #
+    # . # . #
+    # . # . #
+    . . . . .
+    . # # # .
+    . # # # .
+    . . # . .
+    . . # . .
+    `))
 basic.forever(function () {
-	
+    serial.writeLine("Luz: " + input.lightLevel())
+    serial.writeLine("Temperatura" + input.temperature())
+    serial.writeLine("Sonido" + "Mundo")
+    basic.pause(500)
+    if (pantalla == 1) {
+        makerbit.lcdShowCharacter1602(LcdChar.c1, makerbit.position1602(LcdPosition1602.Pos17))
+        makerbit.showStringOnLcd1602("Luz: ", makerbit.position1602(LcdPosition1602.Pos19), 6)
+        makerbit.showStringOnLcd1602("" + (pins.map(
+        input.lightLevel(),
+        0,
+        255,
+        0,
+        100
+        )), makerbit.position1602(LcdPosition1602.Pos28), 4)
+        makerbit.showStringOnLcd1602("%", makerbit.position1602(LcdPosition1602.Pos32), 6)
+    } else if (pantalla == 2) {
+        makerbit.lcdShowCharacter1602(LcdChar.c2, makerbit.position1602(LcdPosition1602.Pos17))
+        makerbit.showStringOnLcd1602("Temperatura:  ", makerbit.position1602(LcdPosition1602.Pos19), 6)
+        makerbit.showStringOnLcd1602("" + (pins.map(
+        input.temperature(),
+        0,
+        255,
+        0,
+        100
+        )), makerbit.position1602(LcdPosition1602.Pos28), 4)
+        makerbit.showStringOnLcd1602("%", makerbit.position1602(LcdPosition1602.Pos32), 6)
+    } else {
+        makerbit.lcdShowCharacter1602(LcdChar.c3, makerbit.position1602(LcdPosition1602.Pos17))
+        makerbit.showStringOnLcd1602("Sonido: ", makerbit.position1602(LcdPosition1602.Pos19), 6)
+        makerbit.showStringOnLcd1602("" + (pins.map(
+        input.soundLevel(),
+        0,
+        255,
+        0,
+        100
+        )), makerbit.position1602(LcdPosition1602.Pos28), 4)
+        makerbit.showStringOnLcd1602("%", makerbit.position1602(LcdPosition1602.Pos32), 6)
+    }
 })
